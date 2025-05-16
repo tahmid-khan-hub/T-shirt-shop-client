c42d293432801587b0446312ad6f4f55770e60bb
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-16 22:28:45 +0600

    updated home and add t-shirt section

--- src/components/AddTshirt.jsx
+++ src/components/AddTshirt.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const AddTshirt = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+        console.log(data);
+    }
+
+  return (
+    <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl mt-24">
+      <div className="card-body">
+        <h1 className="text-3xl mb-7 text-center font-bold">Add T-shirt here</h1>
+        <form onSubmit={handleSubmit} className="fieldset">
+
+          <label className="label">color</label>
+          <input type="text" className="input" name="color" placeholder="color" />
+          <label className="label">quantity</label>
+          <input type="text" className="input" name="quantity" placeholder="quantity" />
+          <label className="label">price</label>
+          <input type="text" className="input" name="price" placeholder="price" />
+          <label className="label">quality</label>
+          <input type="text" className="input" name="quality" placeholder="quality" />
+          <label className="label">photo</label>
+          <input type="text" className="input" name="photo" placeholder="photo" />
+          
+          <button className="btn  btn-neutral mt-4">Add T-shirt</button>
+
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddTshirt;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 
 const Home = () => {
     return (
-        <div>
+        <div className='w-11/12 md:max-w-[1300px] mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
